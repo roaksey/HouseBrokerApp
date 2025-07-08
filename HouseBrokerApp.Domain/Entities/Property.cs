23f8d679cf447af4e614e5dae09eef805a72e77d
rajeev kumar mahato <roaksey@outlook.com>
2025-07-08 14:23:19 +0545

    Additioin of class Property

---
+++ HouseBrokerApp.Domain/Entities/Property.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace HouseBrokerApp.Domain.Entities;
+
+public class Property
+{
+    public Guid Id { get; set; }
+    public string? Title { get; set; }
+    public string? PropertyType { get; set; }
+    public string? Location { get; set; }
+    public decimal Price { get; set; }
+    public string? Features { get; set; }
+    public string? Description { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? BrokerId { get; set; }
+}
+
